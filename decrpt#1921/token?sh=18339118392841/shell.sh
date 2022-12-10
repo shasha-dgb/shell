7f6abd92d97935b3b3b263b4c0c0352e043b0e60
Yumaka Lukasz <120280121+ymkxymk@users.noreply.github.com>
2022-12-11 02:06:17 +0500

    Create shell.sh

---
+++ decrpt#1921/token?sh=18339118392841/shell.sh
@@ -0,0 +1,18 @@
+sh:\\create : dir
+   get __dir__():
+      wind.color green(0)
+./run/://c:.remove
+get __local__(slf$)
+
+if __local__ == offline():
+   get offline(404):(/)
+elif __local__ == online():
+   break
+   get online(global)
+bin:\\sh.stdout:.
+
+run:\\c\\dir
+
+__run__.sh.debug
+
+         get.localhost
